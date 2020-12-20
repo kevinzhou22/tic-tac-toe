df9ab3354a51c1d6b66d05473a4e6751d9661f70
Kevin Zhou <kevinzhou22@outlook.com>
2020-12-20 14:13:58 +1100

    Flesh out DOMController module
    Code all DOMController methods and initialization code for the module.
    Add event listeners to the UI's elements to handle input and process
    that input.

--- script.js
+++ script.js
@@ -8,37 +8,119 @@ const playerFactory = function(name, marker,) {
 
 
 const DOMController = (function() {
+
+    // cache DOM
+    const newGameContainer = document.querySelector(".new-game-container");
+    const board = document.querySelector(".board");
+    const displayText = document.querySelector(".announcer");
+
      /* updates the UI for the current state of the board */
      const render = function(currentState) {
-         console.table(currentState);
+        const squares = board.querySelectorAll(".square");
+        squares.forEach((square) => {
+            const positions = square.getAttribute("data-position").split("-");
+            const x = positions[0];
+            const y = positions[1];
+            square.textContent = currentState[x][y] === null ? "":
+                    currentState[x][y].toUpperCase();
+        });
     };
 
+    /* Updates the text above the board. If either argument is null,
+    it is not rendered. If both are present, first enters the boldPortion,
+    then the nonBoldPortion */
+    const _updateDisplayText = function(boldPortion, nonBoldPortion) {
+        displayText.textContent = "";
+        if(boldPortion !== null) {
+            const playerName = document.createElement("span");
+            playerName.textContent = boldPortion;
+            playerName.style.fontWeight = "bold";
+            displayText.appendChild(playerName);
+        }
+        if(nonBoldPortion !== null) {
+            const restOfText = document.createTextNode(nonBoldPortion);
+            displayText.appendChild(restOfText);
+        }
+
+    };
+
+    /* Updates displayText to accounce a victory and prevents further moves*/
     const displayVictory = function(victorName) {
-        console.log(`${victorName} wins`);
+        _updateDisplayText(victorName, " wins!");
+        board.newGameContainer.display = "block";
     }
-    const displayDraw = function() {
-        console.log("DRAW");
 
+    /* Updates displayText to accounce a draw and prevents further moves*/
+    const displayDraw = function() {
+        _updateDisplayText(null, "It's a draw!");
+        board.newGameContainer.display = "block";
     };
+
+
     const resetDisplay = function() {
-        console.log("DISPLAY RESET");
+        const emptyBoard = [];
+        for(let i = 0; i < 3; i++) {
+            emptyBoard.push([null,null,null]);
+        }
+        render(emptyBoard);
     };
 
     const switchTurn = function(name) {
-        console.log("TURN SWITCH")
+        _updateDisplayText(name, ", it's your turn!");
     };
+
+    const _onClickOfSquare = function(e) {
+        const positions = e.currentTarget.getAttribute("data-position").split("-");
+        const x = positions[0];
+        const y = positions[1];
+        gameController.handleMoveSelection(x,y);
+    };
+
+    const _onClickOfNewGameButton = function(e) {
+        e.currentTarget.parentNode.style.display = "none";
+    };
+
+    const initialize = function(player1Name) {        
+
+        // set up initial text
+        const playerName = document.createElement("span");
+        playerName.textContent = player1Name;
+        playerName.style.fontWeight = "bold";
+        displayText.textContent = "";
+        displayText.appendChild(playerName);
+        const restOfText = document.createTextNode(", it's your turn!");
+        displayText.appendChild(restOfText);
+
+        // add squares and append event listener
+        for(let y = 0; y < 3; y++) {
+            for(let x = 0; x < 3; x++) {
+                const square = document.createElement("div");
+                square.classList.add("square");
+                square.setAttribute("data-position",`${x}-${y}`);
+                board.appendChild(square);
+                square.addEventListener("click",_onClickOfSquare);
+            }
+        }
+
+        //add event listener to make newGameContainer close when the new-game-button is clicked
+        newGameContainer.querySelector(".new-game-button").addEventListener("click",_onClickOfNewGameButton);
+    }
+
+    
     return {
         render,
         displayVictory,
         switchTurn,
         resetDisplay,
         displayDraw,
+        initialize,
     }
 })();
 
 const gameBoard = (function() {
-    const currentState = [[],[],[]];
+    const currentState = [];
     for(let i = 0; i < 3; i ++) {
+        currentState[i] = [];
         for (let j = 0; j < 3; j++) {
             currentState[i][j] = null;
         }
@@ -106,7 +188,6 @@ const gameBoard = (function() {
         if(_isValid(x,y)) {
             currentState[x][y] = marker;
             DOMController.render(currentState);
-            return true;
         } else {
             return false;
         }
@@ -127,6 +208,8 @@ const gameController = (function() {
     players[1] = playerFactory("Player 2","o");
 
     let currentPlayer = players[0];
+    DOMController.initialize(currentPlayer.name);
+
 
     /* takes a position (x,y) and attempts to make that move on the
     gameBoard. If successful, _handlesSelection will either declare victory
